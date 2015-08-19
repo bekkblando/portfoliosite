7959ca992f2532aba7a3a626d08d774f214fe6fb
Bekk Blando <bekkblando@gmail.com>
2015-08-19 19:55:03 -0400

    Turned debug off

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+ratc$^5yx!#*7y_!xt9jfys^ba_=vf-&2p4#9axan+lvv2))+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
