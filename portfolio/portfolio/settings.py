309124259b134cb060da3fc1490863e69d31c827
Bekk Blando <bekkblando@gmail.com>
2016-01-30 14:01:06 -0500

    Fixing the os.environ debug goes to str which evalutes to truthy

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["django_secret_bekkblando"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["django_debug"]
+DEBUG = eval(os.environ["django_debug"])
 
 ALLOWED_HOSTS = ['*']
 
